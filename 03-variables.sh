cc178a2740ce760e046a6c90516a2855eddca01e
Venkatasivareddy-dev <kvsivareddy98@gmail.com>
2026-03-27 13:26:05 +0530

    new script added

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=Siva
+PERSON2=Venkat
+
+
+echo "$PERSON1 : Hi $PERSON2"
+echo "$PERSON2 : Hi $PERSON1"
+echo "$PERSON1 : How are you"
+echo "$PERSON2 : Im good Thanks how are you"
+echo "$PERSON1 : Im Good Thanks"
